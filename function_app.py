d25497dcd71f9f600339b15c4576b5b8207af267
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-07-11 18:13:58 +0530

    added two pipelines

--- function_app.py
+++ function_app.py
@@ -19,9 +19,9 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP2 triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP3 triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP3 triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
