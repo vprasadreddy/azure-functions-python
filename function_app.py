9977156678ee3f52daa3807b49142c60061cab36
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-07-11 18:04:29 +0530

    added pipeline

--- function_app.py
+++ function_app.py
@@ -19,7 +19,7 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP2 triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
