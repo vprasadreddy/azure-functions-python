dabab840dca099a26b0676f3ef6ee28d5780eb88
vara prasad reddy jaggu <prasad.reddy0708@gmail.com>
2024-07-22 13:03:02 +0530

    updated function

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 @app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP5 trigger function processed a request.')
 
     name = req.params.get('name')
     if not name:
@@ -19,9 +19,9 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP4 triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP5 triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP4 triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP5 triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
